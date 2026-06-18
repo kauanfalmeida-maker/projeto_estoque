99a5988b8fd0d2d32b5c2916cb5858b85b68981e
kauanfalmeida-maker <kauan.f.almeida@edu.senai.br>
2026-06-18 09:44:17 -0300

    Estou na metade do codigo

---
+++ Projeto_Estoque.py
@@ -0,0 +1,44 @@
+# [id, nome, quantidade, localização]
+
+estoque = [
+[1, "Amortecedor",3,"Pratileira 01"],
+[2, "Fluido de freio",5,"Pratileira 02"],
+[3, "Carburador",4,"Pratileira 03"],
+[4, "Balança Traseira",6,"Pratileira 04"]
+]
+for linha in estoque:
+ print(linha)
+
+
+def Mostrar_produtinho():
+    ##Essa funcao pergunta o nome do tripulante e adiciona na lista de tripulantes
+    nomeProduto = input("QUAL O NOME DO PRODUTO?: ")
+    idProduto = input("QUAL O ID DO PRODUTO?: ")
+    quantidadeProduto = input("QUAL A QUANTIDADE DO PRODUTO?: ")
+    localizaçãoProduto = input("QUAL A LOCALIZAÇÃO DO PRODUTO?: ")
+    estoque.append([idProduto, nomeProduto, quantidadeProduto, localizaçãoProduto])
+    print("RESGISTRO CONCLUIDO! ")
+
+def lista_produtos():
+    print("----- PRODUTINHOS -----")
+    print(f"TEMOS {estoque} DISPONIVEL: ")
+    print("-------------------------- \n")
+
+def idProduto():
+    idProduto = int(input("QUAL O ID DO PRODUTO:"))
+
+while True:
+    print("\nBem vindo ao menu do estoque. Por favor selecione uma opção:")
+    print("\n1- lista_produtos | 2- idProduto | 3- Mostrar Produtinho | 4- Atualizar | 5- Sair:")
+    opcao = input("Escolha: ")
+    if (opcao == "1"):
+        lista_produtos()
+    elif (opcao == "2"):
+        idProduto()
+    elif (opcao =="3"):
+        Mostrar_produtinho()
+    elif (opcao =="4"):
+        ()
+ 
+ 
+  
